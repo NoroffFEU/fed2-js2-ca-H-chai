8ad3e03a159abab48aa2a9e19f95e5387ea42236
H-chai <143179912+H-chai@users.noreply.github.com>
2024-11-05 08:57:29 +0100

    change logo img and apply tailwind css to auth pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./**/*.{html,js}", "!./node_modules/**/*"],
   theme: {
+    fontFamily: {
+      display: ["Clash Display", "sans-serif"],
+      body: ["Archivo", "sans-serif"],
+    },
+    colors: {
+      "bg-light": "#f2f2eb",
+      green: "#025951",
+      white: "#ffffff",
+      gray: "#4a5568",
+    },
     extend: {},
   },
   plugins: [],
